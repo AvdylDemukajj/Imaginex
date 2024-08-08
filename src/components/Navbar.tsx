eb87d4d1925dfd2b072c2a4849b12358eaac9d1f
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-08 19:40:33 +0200

    "adding search input filed and some styles for it"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar =() =>{
             <Link href="/" className='font-bold text-xl text-blue-800'>IMAGINEX</Link>
         </div>
         {/* CENTER */}
-        <div className='hidden md:flex w-[50%] text-sm'>
+        <div className='hidden md:flex w-[50%] text-sm items-center justify-between'>
             <div className='flex gap-6 text-gray-600'>
                 <Link href="/" className='flex gap-2 items-center'>
                     <Image src="/home.png" alt="Homepage" width={16} height={16} className='w-4 h-4'/>
@@ -27,6 +27,10 @@ const Navbar =() =>{
                     <span>Stories</span>
                 </Link>
             </div>
+            <div className='hidden xl:flex p-2 bg-slate-100 items-center rounded-xl'>
+                <input type="text" placeholder='Search...' className='bg-transparent outline-none' />
+                <Image src="/search.png" alt="earch" width={14} height={14} />
+            </div>
         </div>
         {/* RIGHT */}
         <div className='w-[30%] flex items-center gap-4 xl:gap-8 justify-end'>
@@ -36,7 +40,7 @@ const Navbar =() =>{
             <ClerkLoaded>
                 <SignedIn> 
                     <div className='curser-pointer'>
-                        <Image src="/people.png" alt='People' width={20} height={20} />
+                        <Image src="/people.png" alt='People' width={24} height={24} />
                     </div>
                     <div className='curser-pointer'>
                         <Image src="/messages.png" alt='Messages' width={20} height={20} />
