55b71cc75b24accead995f4fe2999898a9ebd5f6
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-08 19:54:43 +0200

    "adding components and some style to the home page"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
+import AddPost from "@/components/AddPost"
+import Feed from "@/components/Feed"
+import LeftMenu from "@/components/LeftMenu"
+import RightMenu from "@/components/RightMenu"
+import Stories from "@/components/Stories"
+
 const Homepage = () => {
   return (
-    <div className='flex gap-6'>
+    <div className='flex gap-6 pt-6'>
       {/* LEFT */}
       <div className="hidden xl:block w-[20%]">
-
+        <LeftMenu />
       </div>
       {/* CENTER */}
       <div className="w-full lg:w-[70%] xl:w-[50%]">
-
+        <div className="flex gap-6 flex-col">
+          <Stories />
+          <AddPost />
+          <Feed />
+        </div>
       </div>
       {/* RIGHT */}
       <div className="hidden lg:block w-[30%]">
-
+        <RightMenu />
       </div>
       
     </div>
