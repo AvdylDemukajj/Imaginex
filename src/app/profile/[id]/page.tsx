0101010a5039555a474bcc34cd1027a8d7f999aa
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-11 13:07:13 +0200

    "adding structure of user profile page"

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,8 +1,26 @@
+import Feed from '@/components/Feed'
+import LeftMenu from '@/components/LeftMenu'
+import RightMenu from '@/components/RightMenu'
 import React from 'react'
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <div className='flex gap-6 pt-6'>
+    {/* LEFT */}
+    <div className="hidden xl:block w-[20%]">
+      <LeftMenu />
+    </div>
+    {/* CENTER */}
+    <div className="w-full lg:w-[70%] xl:w-[50%]">
+      <div className="flex gap-6 flex-col">
+        <Feed />
+      </div>
+    </div>
+    {/* RIGHT */}
+    <div className="hidden lg:block w-[30%]">
+      <RightMenu userId='test' />
+    </div>
+  </div>
   )
 }
 
