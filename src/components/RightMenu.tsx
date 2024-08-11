b550be4abefda6cd7c1e12f18280bff640fedbcd
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-11 13:06:36 +0200

    "adding userinfo and user media for profile part"

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -2,13 +2,21 @@ import React from 'react'
 import FriendRequests from './FriendRequests'
 import Birthday from './Birthday'
 import Ad from './Ad'
+import UserInfoCard from './UserInfoCard'
+import UserMediaCard from './UserMediaCard'
 
 const RightMenu = ({ userId }:{ userId?:string }) => {
   return (
     <div className='flex flex-col gap-6'>
+      {userId ? (
+        <>
+          <UserInfoCard userId={userId}/>
+          <UserMediaCard userId={userId }/>
+        </>
+      ) : null}
       <FriendRequests />
       <Birthday />
-      <Ad />
+      <Ad size="md"/>
     </div>
   )
 }
