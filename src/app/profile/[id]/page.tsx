516305b5e339ca9d4460fb1d4d3648482c2da41d
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-11 22:32:06 +0200

    "profile page design"

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Feed from '@/components/Feed'
 import LeftMenu from '@/components/LeftMenu'
 import RightMenu from '@/components/RightMenu'
+import Image from 'next/image'
 import React from 'react'
 
 const ProfilePage = () => {
@@ -13,6 +14,27 @@ const ProfilePage = () => {
     {/* CENTER */}
     <div className="w-full lg:w-[70%] xl:w-[50%]">
       <div className="flex gap-6 flex-col">
+        <div className='lex flex-col items-center justify-cente'>
+          <div className='w-full h-full relative'>
+            <Image src="/" alt="CoverPicture" fill className=' rounded-md object-cover' />
+            <Image src="/" alt="ProfilePicture" width={128} height={128} className='w-32 h-32 rounded-full absolute left-0 right0 m-auto -bottom-16 ring-4 ring-white  object-cover' />
+          </div>
+          <h1 className='mt-20 mb-4 text-2xl font-medium'>User Name</h1>
+          <div className='flex items-center justify-center gap-12 mb-4'>
+            <div className='flex flex-col items-center'>
+              <span className='font-medium'>123</span>
+              <span className='text-sm'>Posts</span>
+            </div>
+            <div className='flex flex-col items-center'>
+              <span className='font-medium'>123</span>
+              <span className='text-sm'>Followers</span>
+            </div>
+            <div className='flex flex-col items-center'>
+              <span className='font-medium'>123</span>
+              <span className='text-sm'>Following</span>
+            </div>
+          </div>
+        </div>
         <Feed />
       </div>
     </div>
