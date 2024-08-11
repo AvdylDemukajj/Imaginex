ae3a66ff5c2d3ee6023098016315411637fe22c9
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-11 18:39:59 +0200

    "design for the user media card"

--- src/components/UserMediaCard.tsx
+++ src/components/UserMediaCard.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-const UserMediaCard = () => {
+const UserMediaCard = ({userId}: {userId: string}) => {
   return (
-    <div>UserMediaCard</div>
+    <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'>
+    {/* TOP */}
+        <div className='flex justify-between items-center font-medium'>
+            <span className='text-gray-500'>User Media</span>
+            <Link href="/" className='text-blue-500 text-xs'>See all</Link>
+        </div>
+    {/* BOTTOM */}
+        <div className='flex gap-4 justify-between flex-wrap'>
+          <div className='relative w-1/5 h-24'>
+            <Image src='/' alt='RequestFriend' fill className='object-contain rounded-md'/>
+          </div>
+
+        </div>
+    </div>
   )
 }
 
