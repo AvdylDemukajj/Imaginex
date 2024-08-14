a37f8950a05d6a4f0dc96a4369260d2ab2dc6d4e
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-15 00:10:16 +0200

    "make data dynamic"

--- src/components/rightMenu/FriendRequestList.tsx
+++ src/components/rightMenu/FriendRequestList.tsx
@@ -12,8 +12,8 @@ const FriendRequestList =({requests}:{requests: RequestWithUser[]}) => {
         {requests.map(request => (
         <div className='flex items-center justify-between' key={request.id}>
             <div className='flex items-center gap-4'>
-                <Image src="/" alt='' width={40} height={40} className='w-10 h-10 rounded-full object-cover'/>
-                <span className='font-semibold'>User 1</span>
+                <Image src={request.sender.avatar || "/noAvatar.png"} alt='' width={40} height={40} className='w-10 h-10 rounded-full object-cover'/>
+                <span className='font-semibold'>{(request.sender.name && request.sender.surname) ? request.sender.name + " " + request.sender.surname : request.sender.username}</span>
             </div>
             <div className='flex gap-3 justify-end'>
             <Image src="/accept.png" alt='' width={20} height={20} className='cursor-pointer'/>
