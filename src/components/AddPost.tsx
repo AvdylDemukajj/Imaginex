85851ce468cea76a47d29da84f5d41ebd1790f45
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-14 21:03:30 +0200

    "changing prompts from userid to user"

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,7 +1,12 @@
+import prisma from '@/lib/prisma';
+import { auth } from '@clerk/nextjs/server';
 import Image from 'next/image'
 import React from 'react'
 
 const AddPost = () => {
+
+    const { userId } = auth()
+
   return (
     <div className='p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm'>
         {/* AVATAR */}
@@ -9,10 +14,11 @@ const AddPost = () => {
         {/* POST */}
         <div className='flex-1'>
             {/* TEXT INPUT */}
-            <div className='flex gap-4'>
-                <textarea placeholder="What's on your mind?" className='bg-slate-100 rounded-lg flex-1 p-2'></textarea>
+            <form action="" className='flex gap-4'>
+                <textarea placeholder="What's on your mind?" className='bg-slate-100 rounded-lg flex-1 p-2' name='desc'></textarea>
                 <Image src='/emoji.png' alt='Avatar' width={20} height={20} className='w-5 h-5 cursor-pointer self-end'/>
-            </div>
+                <button>Send</button>
+            </form>
             {/* POST OPSIONS */}
             <div className='flex items-center gap-4 mt-4 text-gray-400 flex-wrap'>
                 <div className='flex items-center gap-2 cursor-pointer'>
