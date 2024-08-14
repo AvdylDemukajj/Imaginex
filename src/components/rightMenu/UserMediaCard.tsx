d3465ed10940d132ad2c512eac9fd917eaeb85ac
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-14 23:14:47 +0200

    "adding posts dynamically"

--- src/components/rightMenu/UserMediaCard.tsx
+++ src/components/rightMenu/UserMediaCard.tsx
@@ -1,9 +1,24 @@
+import prisma from '@/lib/prisma'
 import { User } from '@prisma/client'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const UserMediaCard = ({user}: {user: User}) => {
+const UserMediaCard = async ({user}: {user: User}) => {
+
+   const postsWithMedia = await prisma.post.findMany({
+     where: {
+       userId: user.id,
+       img: {
+        not: null,
+       }
+     },
+     take: 8,
+     orderBy: {
+      createdAt: 'desc'
+     }
+   })
+
   return (
     <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'>
     {/* TOP */}
@@ -13,10 +28,10 @@ const UserMediaCard = ({user}: {user: User}) => {
         </div>
     {/* BOTTOM */}
         <div className='flex gap-4 justify-between flex-wrap'>
-          <div className='relative w-1/5 h-24'>
-            <Image src='/' alt='' fill className='object-contain rounded-md'/>
-          </div>
-
+          {postsWithMedia.length ? postsWithMedia.map(post=>( <div className='relative w-1/5 h-24' key={post.id}>
+            <Image src={post.img!} alt='' fill className='object-contain rounded-md'/>
+          </div>))
+          : "No media found!"}
         </div>
     </div>
   )
