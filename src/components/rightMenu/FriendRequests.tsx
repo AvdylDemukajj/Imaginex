a8a94358e45cc722fb5d2aec84591100836cd283
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-15 00:05:41 +0200

    "fatching data dynamically and creating a new components for our request list"

--- src/components/rightMenu/FriendRequests.tsx
+++ src/components/rightMenu/FriendRequests.tsx
@@ -1,8 +1,27 @@
+import prisma from '@/lib/prisma'
+import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import FriendRequestList from './FriendRequestList'
 
-const FriendRequests = () => {
+const FriendRequests = async() => {
+
+  const {userId} = auth()
+
+  if(!userId) return null;
+
+  const requests = await prisma.followRequest.findMany({
+    where:{
+      receiverId: userId,
+    },
+    include:{
+      sender: true,
+    }
+  })
+
+  if(requests.length === 0) return null;
+
   return (
     <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'>
         {/* TOP */}
@@ -11,16 +30,7 @@ const FriendRequests = () => {
             <Link href="/" className='text-blue-500 text-xs'>See all</Link>
         </div>
         {/* USER */}
-        <div className='flex items-center justify-between'>
-            <div className='flex items-center gap-4'>
-                <Image src="/" alt='UserRequestProfilePic' width={40} height={40} className='w-10 h-10 rounded-full object-cover'/>
-                <span className='font-semibold'>User 1</span>
-            </div>
-            <div className='flex gap-3 justify-end'>
-            <Image src="/accept.png" alt='AcceptIcon' width={20} height={20} className='cursor-pointer'/>
-            <Image src="/reject.png" alt='RejectIcon' width={20} height={20} className='cursor-pointer'/>
-            </div>
-        </div>
+        <FriendRequestList requests={requests}/>
     </div>
   )
 }
