775053f02069e13c861fca3127bee36e434442b3
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-17 19:25:59 +0200

    "method to get posts"

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -49,6 +49,30 @@ const Feed = async({username}:{username?: string}) => {
 
   const followingIds = following.map((f) => f.followingId);
 
+  posts = await prisma.post.findMany({
+    where: {
+      userId: {
+        in: followingIds,
+      },
+    },
+    include: {
+      user: true,
+      likes: {
+        select: {
+          userId: true,
+        },
+      },
+      _count: {
+        select: {
+          comments: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
 
     return (
       <div className='p-4 bg-white shadow-md rounded-lg flex flex-col gap-12'>
