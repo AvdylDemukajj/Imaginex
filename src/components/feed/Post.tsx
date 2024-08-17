fc6fc267c2b20e229b0a0be9e748355be9dbd9ce
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-17 19:36:44 +0200

    "adding posts as a prompt and creatig feed post type"

--- src/components/feed/Post.tsx
+++ src/components/feed/Post.tsx
@@ -1,23 +1,25 @@
 import Image from 'next/image'
 import React from 'react'
 import Comments from './Comments'
+import { Post as PostType, User } from '@prisma/client'
 
+type FeedPostType = PostType & {user: User} & {likes:[{userId:string}]} & {_counts:{comments:number}}
 
-const Post = () => {
+const Post = ({post}:{post: FeedPostType}) => {
   return (
     <div className='fle flex-col gap-4'>
         {/* USER */}
         <div className='flex items-center justify-between'>
             <div className='flex items-center gap-4'>
-                <Image src="" alt='UserImage' width={40} height={40} className='w-10 h-10 rounded-full'/>
+                <Image src="" alt='' width={40} height={40} className='w-10 h-10 rounded-full'/>
                 <span className='font-medium'>Lumi</span>
             </div>
-            <Image src="/more.png" alt='More' width={16} height={16} />
+            <Image src="/more.png" alt='' width={16} height={16} />
         </div>
         {/* DESCRIPTION */}
         <div className='flex flex-col gap-4'>
             <div className='w-full min-h-96 relative'>
-                <Image src="" alt='Post' fill className='object-cover rounded-md' />
+                <Image src="" alt='' fill className='object-cover rounded-md' />
             </div>
             <p>1212121212</p>
         </div>
@@ -25,19 +27,19 @@ const Post = () => {
         <div className='flex items-center justify-between text-sm my-4'>
             <div className='flex gap-8'>
                 <div className='flex items-center gap-4 bg-slate-50 p-2 rounded-xl'>
-                    <Image src="/like.png" alt='Like' width={16} height={16} className='cursor-pointer' />
+                    <Image src="/like.png" alt='' width={16} height={16} className='cursor-pointer' />
                     <span className='text-gray-300'>|</span>
                     <span className='text-gray-500'>123 <span className='hidden md:inline'> Likes</span></span>
                 </div>
                 <div className='flex items-center gap-4 bg-slate-50 p-2 rounded-xl'>
-                    <Image src="/comment.png" alt='Coment' width={16} height={16} className='cursor-pointer' />
+                    <Image src="/comment.png" alt='' width={16} height={16} className='cursor-pointer' />
                     <span className='text-gray-300'>|</span>
                     <span className='text-gray-500'>123 <span className='hidden md:inline'> Comments</span></span>
                 </div>
             </div>
             <div className=''>
                 <div className='flex items-center gap-4 bg-slate-50 p-2 rounded-xl'>
-                        <Image src="/share.png" alt='Share' width={16} height={16} className='cursor-pointer' />
+                        <Image src="/share.png" alt='' width={16} height={16} className='cursor-pointer' />
                         <span className='text-gray-300'>|</span>
                         <span className='text-gray-500'>123 <span className='hidden md:inline'> Shares</span></span>
                 </div>
