ce2142f6bd0e1f408b94b590dd7747ff687ec4e7
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-17 23:42:21 +0200

    "fatching data dynamic"

--- src/components/feed/Post.tsx
+++ src/components/feed/Post.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import Comments from './Comments'
 import { Post as PostType, User } from '@prisma/client'
+import PostInteraction from './PostInteraction'
 
 type FeedPostType = PostType & {user: User} & {likes:[{userId:string}]} & {_counts:{comments:number}}
 
@@ -11,40 +12,22 @@ const Post = ({post}:{post: FeedPostType}) => {
         {/* USER */}
         <div className='flex items-center justify-between'>
             <div className='flex items-center gap-4'>
-                <Image src="" alt='' width={40} height={40} className='w-10 h-10 rounded-full'/>
-                <span className='font-medium'>Lumi</span>
+                <Image src={post.user.avatar || "/noAvatar.png"} alt='' width={40} height={40} className='w-10 h-10 rounded-full'/>
+                <span className='font-medium'>{(post.user.name && post.user.surname) ? post.user.name +" "+ post.user.surname : post.user.username}</span>
             </div>
             <Image src="/more.png" alt='' width={16} height={16} />
         </div>
         {/* DESCRIPTION */}
         <div className='flex flex-col gap-4'>
-            <div className='w-full min-h-96 relative'>
-                <Image src="" alt='' fill className='object-cover rounded-md' />
-            </div>
-            <p>1212121212</p>
+           {post.img && <div className='w-full min-h-96 relative'>
+                <Image src={post.img} alt='' fill className='object-cover rounded-md' />
+            </div>}
+            <p>
+                {post.desc}
+            </p>
         </div>
         {/* INTERACTION */}
-        <div className='flex items-center justify-between text-sm my-4'>
-            <div className='flex gap-8'>
-                <div className='flex items-center gap-4 bg-slate-50 p-2 rounded-xl'>
-                    <Image src="/like.png" alt='' width={16} height={16} className='cursor-pointer' />
-                    <span className='text-gray-300'>|</span>
-                    <span className='text-gray-500'>123 <span className='hidden md:inline'> Likes</span></span>
-                </div>
-                <div className='flex items-center gap-4 bg-slate-50 p-2 rounded-xl'>
-                    <Image src="/comment.png" alt='' width={16} height={16} className='cursor-pointer' />
-                    <span className='text-gray-300'>|</span>
-                    <span className='text-gray-500'>123 <span className='hidden md:inline'> Comments</span></span>
-                </div>
-            </div>
-            <div className=''>
-                <div className='flex items-center gap-4 bg-slate-50 p-2 rounded-xl'>
-                        <Image src="/share.png" alt='' width={16} height={16} className='cursor-pointer' />
-                        <span className='text-gray-300'>|</span>
-                        <span className='text-gray-500'>123 <span className='hidden md:inline'> Shares</span></span>
-                </div>
-            </div>
-        </div>
+        <PostInteraction postId={post.id} likes={post.likes.map((like) => like.userId)} commentNumber={post._counts.comments} />
         <Comments />
     </div>
   )
