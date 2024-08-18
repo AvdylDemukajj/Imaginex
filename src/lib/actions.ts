89347b494341bd04216a95978f3444d1d05680a4
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-18 21:48:52 +0200

    "add post action to create a post"

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,6 +3,7 @@
 import { auth } from "@clerk/nextjs/server"
 import prisma from "./prisma"
 import { z } from "zod";
+import { revalidatePath } from "next/cache";
 
 
 export const switchFollow = async (userId:string) => {
@@ -257,5 +258,36 @@ export const switchBlock = async (userId: string) => {
     } catch (err) {
       console.log(err);
       throw new Error("Something went wrong!");
+    }
+  };
+
+  export const addPost = async (formData: FormData, img: string) => {
+    const desc = formData.get("desc") as string;
+  
+    const Desc = z.string().min(1).max(255);
+  
+    const validatedDesc = Desc.safeParse(desc);
+  
+    if (!validatedDesc.success) {
+      //TODO
+      console.log("description is not valid");
+      return;
+    }
+    const { userId } = auth();
+  
+    if (!userId) throw new Error("User is not authenticated!");
+  
+    try {
+      await prisma.post.create({
+        data: {
+          desc: validatedDesc.data,
+          userId,
+          img,
+        },
+      });
+  
+      revalidatePath("/");
+    } catch (err) {
+      console.log(err);
     }
   };
