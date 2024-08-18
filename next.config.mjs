470d655faae7388eae14166653ecfa66ee96f427
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-18 22:18:54 +0200

    "adding pexels images"

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,24 @@
 const nextConfig = {
   experimental: {
     reactCompiler: true,
-    ppr: 'incremental'
+    ppr: "incremental",
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
+        protocol: "https",
+        hostname: "images.pexels.com",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
