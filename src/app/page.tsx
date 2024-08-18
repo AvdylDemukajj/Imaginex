166638d917e3577351e0b8e64a144a4010794022
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-18 22:29:33 +0200

    "some small changes and some error fixes"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,28 @@
-import AddPost from "@/components/AddPost"
-import Feed from "@/components/feed/Feed"
-import LeftMenu from "@/components/leftMenu/LeftMenu"
-import RightMenu from "@/components/rightMenu/RightMenu"
-import Stories from "@/components/Stories"
+
+import AddPost from "@/components/AddPost";
+import Stories from "@/components/Stories";
+import Feed from "@/components/feed/Feed";
+import LeftMenu from "@/components/leftMenu/LeftMenu";
+import RightMenu from "@/components/rightMenu/RightMenu";
 
 const Homepage = () => {
   return (
-    <div className='flex gap-6 pt-6'>
-      {/* LEFT */}
+    <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">
         <LeftMenu type="home" />
       </div>
-      {/* CENTER */}
       <div className="w-full lg:w-[70%] xl:w-[50%]">
-        <div className="flex gap-6 flex-col">
+        <div className="flex flex-col gap-6">
           <Stories />
           <AddPost />
           <Feed />
         </div>
       </div>
-      {/* RIGHT */}
       <div className="hidden lg:block w-[30%]">
         <RightMenu />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="h-[calc(100hv-96px)] flex items-center justify-center">
-        <SignIn />
+    <div className="h-[calc(100vh-96px)] flex items-center justify-center">
+      <SignIn />
     </div>
-  )
+  );
 }
