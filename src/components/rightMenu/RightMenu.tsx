b05b9a854e4e51aaf8894755406e9e8c8ffd4fef
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-18 22:21:07 +0200

    "details fix"

--- src/components/rightMenu/RightMenu.tsx
+++ src/components/rightMenu/RightMenu.tsx
@@ -1,29 +1,29 @@
-import React, { Suspense } from 'react'
-import FriendRequests from './FriendRequests'
-import Birthday from './Birthday'
-import Ad from '../Ad'
-import UserInfoCard from './UserInfoCard'
-import UserMediaCard from './UserMediaCard'
-import { User } from '@prisma/client'
+import { User } from "@prisma/client";
+import Ad from "../Ad";
+import Birthdays from "./Birthday";
+import FriendRequests from "./FriendRequests";
+import UserInfoCard from "./UserInfoCard";
+import UserMediaCard from "./UserMediaCard";
+import { Suspense } from "react";
 
-const RightMenu = ({ user }:{ user?:User }) => {
+const RightMenu = ({ user }: { user?: User }) => {
   return (
-    <div className='flex flex-col gap-6'>
+    <div className="flex flex-col gap-6">
       {user ? (
         <>
           <Suspense fallback="loading...">
-            <UserInfoCard user={user}/>
+            <UserInfoCard user={user} />
           </Suspense>
           <Suspense fallback="loading...">
-            <UserMediaCard user={user}/>
+            <UserMediaCard user={user} />
           </Suspense>
         </>
       ) : null}
       <FriendRequests />
-      <Birthday />
-      <Ad size="md"/>
+      <Birthdays />
+      <Ad size="md" />
     </div>
-  )
-}
+  );
+};
 
-export default RightMenu
+export default RightMenu;
