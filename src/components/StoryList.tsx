f27881504207b6797418c8fc29c42491be8eaa52
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-18 22:00:43 +0200

    "using optimistic hook on add functionality"

--- src/components/StoryList.tsx
+++ src/components/StoryList.tsx
@@ -26,6 +26,28 @@ const StoryList = ({
   const add = async () => {
     if (!img?.secure_url) return;
 
+    addOptimisticStory({
+        id: Math.random(),
+        img: img.secure_url,
+        createdAt: new Date(Date.now()),
+        expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        userId: userId,
+        user: {
+          id: userId,
+          username: "Sending...",
+          avatar: user?.imageUrl || "/noAvatar.png",
+          cover: "",
+          description: "",
+          name: "",
+          surname: "",
+          city: "",
+          work: "",
+          school: "",
+          website: "",
+          createdAt: new Date(Date.now()),
+        },
+      });
+      
     try {
       const createdStory = await addStory(img.secure_url);
       setStoryList((prev) => [createdStory!, ...prev]);
